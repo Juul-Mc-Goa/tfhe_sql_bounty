4db062ae2e945215887fac3861a74c08fc8a70db
Juul-Mc-Goa <jules.magois.pro@proton.me>
2024-04-24 18:55:37 +0200

    factor `distinct` logic, add docs

--- src/distinct.rs
+++ src/distinct.rs
@@ -1,6 +1,15 @@
 use crate::{cipher_structs::FheBool, tables::TableQueryRunner};
 
 impl<'a> TableQueryRunner<'a> {
+    /// Given:
+    /// * `index`: an index
+    /// * `projection`: a list of clear booleans encoding which columns are
+    /// selected by that query,
+    /// * `result`: a list of encrypted booleans encoding which entries are
+    /// selected by a `SELECT` query,
+    ///
+    /// Returns the sum of all encrypted booleans `result[i]` such that for all `j`, we have
+    /// `projection[j] = true => result[index][j] = result[i][j]`.
     fn sum_same_projection(
         &'a self,
         index: u8,
@@ -25,48 +34,79 @@ impl<'a> TableQueryRunner<'a> {
             )
     }
 
+    /// An internal, recursive method to do what
+    /// [`is_entry_already_in_result`](Self::is_entry_already_in_result) needs
+    /// to do.
+    ///
+    /// Simply transforms an encrypted `projection` into a `clear_projection` by
+    /// doing a lot of cmuxes, then calls
+    /// [`sum_same_projection`](Self::sum_same_projection) with the clear
+    /// projection.
     fn recursive_cmux_distinct(
         &'a self,
         index: u8,
-        distinct: &'a FheBool<'a>,
         clear_projection: Vec<bool>,
         projection: &[FheBool<'a>],
         result: &[FheBool<'a>],
     ) -> FheBool<'a> {
         if projection.is_empty() {
-            let sum_entries_with_same_proj =
-                self.sum_same_projection(index, clear_projection, result);
-            &sum_entries_with_same_proj * distinct
+            self.sum_same_projection(index, clear_projection, result)
         } else {
             let mut first_proj = clear_projection.clone();
             first_proj.push(false);
             let false_case =
-                self.recursive_cmux_distinct(index, distinct, first_proj, &projection[1..], result);
+                self.recursive_cmux_distinct(index, first_proj, &projection[1..], result);
 
             let mut second_proj = clear_projection;
             second_proj.push(true);
-            let true_case = self.recursive_cmux_distinct(
-                index,
-                distinct,
-                second_proj,
-                &projection[1..],
-                result,
-            );
+            let true_case =
+                self.recursive_cmux_distinct(index, second_proj, &projection[1..], result);
             // cmux(projection[0], true_case, false_case)
             &false_case + &(&projection[0] * &(&true_case + &false_case))
         }
     }
 
+    /// Given:
+    /// * `index`: an index
+    /// * `result`: a list of encrypted booleans encoding which entries are
+    /// selected by a `SELECT` query,
+    /// * `projection`: a list of encrypted booleans encoding which columns are
+    /// selected by that query,
+    ///
+    /// returns a boolean answering the question “does an entry such that
+    /// `result[i] = true` have the same projection as the entry at index `index` ?”
+    ///
+    /// <div class="warning">
+    ///
+    /// This method assumes the following invariant: let `proj(i)` be the tuple
+    /// consisting of all the `self.content[i][j]` such that `projection[j] ==
+    /// true`. Then if `proj(i1) == proj(i2)`, we have either `result[i1] = false` or
+    /// `result[i2] = false`.
+    ///
+    /// This enables replacing `OR`s by `XOR`s in the computation of the result
+    /// boolean. See [`sum_same_projection`](Self::sum_same_projection), and the
+    /// [`crate`](crate#replacing-boolean-operators-with-addition-and-multiplication-mod-2)
+    /// docs for why `XOR` is replaced by `+`.
+    ///
+    /// </div>
     pub fn is_entry_already_in_result(
         &'a self,
         index: u8,
-        distinct: &'a FheBool<'a>,
         projection: &[FheBool<'a>],
         result: &[FheBool<'a>],
     ) -> FheBool<'a> {
-        self.recursive_cmux_distinct(index, distinct, Vec::new(), projection, result)
+        self.recursive_cmux_distinct(index, Vec::new(), projection, result)
     }
 
+    /// Given:
+    /// * `distinct`: an encrypted boolean,
+    /// * `result`: a list of encrypted booleans encoding which entries are
+    /// selected by a `SELECT` query,
+    /// * `projection`: a list of encrypted booleans encoding which columns are
+    /// selected by that query,
+    ///
+    /// Returns a list en encrypted booleans of the same size as `result`, but in which
+    /// duplicates are removed, if `distinct` is true.
     pub fn comply_with_distinct_bool(
         &'a self,
         distinct: &'a FheBool<'a>,
@@ -80,10 +120,11 @@ impl<'a> TableQueryRunner<'a> {
         for (i, result_bool) in result.iter().enumerate() {
             compliant_result[i] = result_bool.clone();
 
-            let is_already_in_result =
-                self.is_entry_already_in_result(i as u8, distinct, projection, &compliant_result);
+            // make_false = distinct AND self.is_entry_already_in_result(...)
+            let make_false =
+                distinct * &self.is_entry_already_in_result(i as u8, projection, &compliant_result);
 
-            compliant_result[i] = &compliant_result[i] * &!is_already_in_result;
+            compliant_result[i] = &compliant_result[i] * &!make_false;
         }
 
         compliant_result
